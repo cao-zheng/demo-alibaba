8a2d8c15103dc22d3dac5a30bc743e6783fcfab9
terran <598197259@qq.com>
2025-04-25 12:01:52 +0800

    no message

--- demo-order/src/main/java/springboot3/demoorder/controller/OrderController.java
+++ demo-order/src/main/java/springboot3/demoorder/controller/OrderController.java
@@ -1,34 +1,53 @@
 package springboot3.demoorder.controller;
 
+import io.seata.spring.annotation.GlobalTransactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import springboot3.demoorder.service.DemoStockService;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 @RestController
 @RequestMapping("/order")
 @RefreshScope
 public class OrderController {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
     @Autowired
     RestTemplate restTemplate;
     @Autowired
     DemoStockService demoStockService;
     @Value("${author}")
     String author;
+    AtomicInteger ordernum = new AtomicInteger(0);
 
     @GetMapping("/add")
+    @GlobalTransactional
     public String add(){
-        String msg = restTemplate.getForObject("http://demo-stock/stock/add", String.class);
+        //订单 + 1
+        String sql = "insert into demo_order values ( " + ordernum.get() + ",'xxx')";
+        jdbcTemplate.update(sql);
+        //库存 - 1
+        String msg = demoStockService.reduce();
+        ordernum.incrementAndGet();
         return author + "hello word" + msg;
     }
 
     @GetMapping("/reduce")
+    @GlobalTransactional
     public String reduce(){
-        String msg = demoStockService.reduce();
+        //订单 - 1
+        String sql = "delete from demo_order where id = " + ordernum.get();
+        jdbcTemplate.update(sql);
+        //库存 + 1
+        String msg = restTemplate.getForObject("http://demo-stock/stock/add", String.class);
+        ordernum.decrementAndGet();
         return author + "hello word" + msg;
     }
 }
--- demo-stock/src/main/java/springboot3/demostock/controller/StockController.java
+++ demo-stock/src/main/java/springboot3/demostock/controller/StockController.java
@@ -1,8 +1,10 @@
 package springboot3.demostock.controller;
 
+import io.seata.spring.annotation.GlobalTransactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +17,8 @@ import java.util.concurrent.atomic.AtomicInteger;
 @RefreshScope
 public class StockController {
     @Autowired
+    private JdbcTemplate jdbcTemplate;
+    @Autowired
     RestTemplate restTemplate;
     AtomicInteger sum = new AtomicInteger(50);
     @Value("${author}")
@@ -22,14 +26,16 @@ public class StockController {
     @GetMapping("/add")
     public String add(){
         int current = sum.incrementAndGet();
-        System.out.println("添加成功");
+        jdbcTemplate.update("insert into demo_stock values (?,?)",current,"123");
+        System.out.println("添加库存成功");
         return author + "剩余货物" + current;
     }
 
     @GetMapping("/reduce")
     public String reduce(){
         int current = sum.decrementAndGet();
-        System.out.println("扣减成功");
+        jdbcTemplate.update("delete from demo_stock where id = ?",current);
+        System.out.println("扣减库存成功");
         return author + "剩余货物" + current;
     }
 }
